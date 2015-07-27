22f9bd0a2f7cab16f101cce3494cc21f43458afc
Jack Forrest <jack@jrforrest.net>
2015-07-27 14:54:28 -0400

    Corrects lipsum text in factories

--- spec/factories/comments.rb
+++ spec/factories/comments.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :comment do
     association :author, factory: :user
-    body { Faker::Lorem.paragraphs(2) }
+    body { Faker::Lorem.paragraphs(2).join("\n\n") }
 
     trait :nested do
       association :parent, factory: :comment
--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
   factory :post do
     association :user
-    content Faker::Lorem.paragraphs(5)
+    content { Faker::Lorem.paragraphs(5).join("\n\n") }
   end
 end
