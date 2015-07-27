91782757074f4f162010d9cb29697b80ce0befb9
Jack Forrest <jack@jrforrest.net>
2015-07-27 16:14:10 -0400

    Allows creating nested comments

--- app/assets/javascripts/posts.coffee
+++ app/assets/javascripts/posts.coffee
@@ -1,3 +1,9 @@
 # Place all the behaviors and hooks related to the matching controller here.
 # All this logic will automatically be available in application.js.
 # You can use CoffeeScript in this file: http://coffeescript.org/
+
+$(document).ready ->
+  $('a.show-comment-form').click (e) ->
+    $(this).parent().children('.new-comment').toggleClass('hidden')
+    $(this).toggleClass('hidden')
+    e.preventDefault()
--- spec/features/create_comment.rb
+++ spec/features/create_comment.rb
@@ -2,20 +2,48 @@ require 'rails_helper'
 
 describe 'Creatinng a post comment' do
   let(:post) { create(:post) }
-  let(:comment_text) { 'Woah there, hello world!' }
-  let(:body_input) { first(:css, "input.text.comment_body") }
+  let(:comment_text) { 'My settlers own Catan.' }
 
-  before do
-    visit post_path(post)
-    fill_in body_input, with: comment_text
-    click_button 'Create Comment'
+  context 'under the post' do
+    let(:body_input) { first(:css, ".comment_body textarea") }
+
+    before do
+      visit post_path(post)
+      body_input.set(comment_text)
+      click_button 'Create Comment'
+    end
+
+    it 'populates the post page with the new comment' do
+      expect(page).to have_content(comment_text)
+    end
+
+    it 'Adds the comment to the post' do
+      expect(post.comments.count).to be > 0
+    end
   end
 
-  it 'populates the post page with the new comment' do
-    expect(page).to have_content(comment_text)
-  end
+  context 'under another comment' do
+    let!(:comment) { create(:comment, parent: post) }
+    let(:nested_body_input) do
+      find(:css, '.comment .new-comment .comment_body textarea')
+    end
+    let(:submit_button) do
+      find(:css, '.comment .new-comment input.button')
+    end
+    let(:nested_comment_text) { 'My settlers own Catan.' }
 
-  it 'Adds the comment to the post' do
-    expect(post.comments.count).to be > 0
+    before do
+      visit post_path(post)
+      nested_body_input.set(nested_comment_text)
+      submit_button.click
+    end
+
+    it 'populates the post page with the new comment' do
+      expect(page).to have_content(nested_comment_text)
+    end
+
+    it 'adds the comment to the parent comment' do
+      expect(comment.comments.count).to be > 0
+    end
   end
 end
